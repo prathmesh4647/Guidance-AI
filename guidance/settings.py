59c14c3f0c4b2bf1ae42f39b3ff4ad4015050f01
Prathmesh Tamboli <prathmeshdtamboli@gmail.com>
2026-04-01 03:04:48 +0530

    updated settings.py

--- guidance/settings.py
+++ guidance/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,10 +83,7 @@ WSGI_APPLICATION = 'guidance.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 DATABASES = {
     'default': {
