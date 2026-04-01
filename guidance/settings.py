18908aac6a3ca8b1cb2333082451ee0d6449143c
Prathmesh Tamboli <prathmeshdtamboli@gmail.com>
2026-04-01 15:10:42 +0530

    MEDIA_ROOT updated

--- guidance/settings.py
+++ guidance/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
 # Ensure your images folder is included
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
@@ -147,7 +148,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 #Media Cha Vishay
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 import os
